c6d93d4438c3309a3fbe4a37d7d5846af2f76d93
Sergio <shtelmah.serhiy@gmail.com>
2021-02-20 10:46:51 +0100

    add move zeroes variant 2 to code

--- src/java/com/arrays/MoveZeroes.java
+++ src/java/com/arrays/MoveZeroes.java
@@ -1,6 +1,19 @@
 package arrays;
 
 public class MoveZeroes {
+    public static void moveZeros2(int[] nums) {
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
     public void moveZeroes(int[] nums) {
         int len = nums.length;
         int count = 0;
