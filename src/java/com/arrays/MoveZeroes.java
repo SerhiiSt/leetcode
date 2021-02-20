985322d78dae85c350df3798dfa35bce60b9790f
Sergio <shtelmah.serhiy@gmail.com>
2021-02-20 10:36:04 +0100

    add move zeroes to code

---
+++ src/java/com/arrays/MoveZeroes.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int len = nums.length;
+        int count = 0;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != 0)
+                nums[count++] = nums[i];
+
+        }
+        while (count < len) {
+            nums[count++] = 0;
+        }
+    }
+}
